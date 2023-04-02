8c19f66747c30cd44f2025903d27c131d273c48a
kz-16 <kzegrya@gmail.com>
2023-04-02 20:36:32 +0300

    Project created

---
+++ Surf Pal/Surf Pal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Surf Pal
+//
+//  Created by KZ on 4/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
